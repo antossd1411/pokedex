08e3d79f4c881da98ef6968052639df722f19295
Antonio Sotillo <antoniosotillo14@hotmail.com>
2023-09-30 20:20:08 -0400

    Adicion de iconos para la barra de navegacion, funcionalidad toggle al presional los items que tienen hijos, estilos de la barra de navegacion

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,8 @@ import { fetchNavItems } from "@/services/navbar";
 import Link from "next/link";
 import { useEffect, useState } from "react"
 import styles from "@/styles/components/Nav.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function Nav() {
     const [navItems, setNavItems] = useState([]);
@@ -17,12 +19,33 @@ export default function Nav() {
             acc.push({
                 name: cur,
                 children: itemChildren,
+                toggled: false,
             });
 
             return acc;
         }, organizedItems);
 
         return organizedItems;
+    }
+
+    const toggleNavItemChildren = (item = "") => {
+        const itemIndex = navItems.findIndex(({ name }) => name === item);
+
+        if (itemIndex > -1) {
+            const currentItems = navItems.map((item, index) => {
+                if (index !== itemIndex && item.toggled) {
+                    item.toggled = false;
+                }
+
+                return item;
+            });
+
+            const isToggled = currentItems[itemIndex].toggled;
+
+            currentItems[itemIndex].toggled = !isToggled;
+
+            setNavItems(currentItems);
+        }
     }
 
     useEffect(() => {
@@ -46,17 +69,33 @@ export default function Nav() {
     }, [])
 
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul className={styles.container}>
+                <p className={styles.closeContainer}>
+                    <FontAwesomeIcon icon={faXmark} className={styles.close} onClick={() => console.log("Close!")} />
+                </p>
+
                 {
                     navItems.map((item) => {
                         return <li key={item.name} className={styles.li}>
-                            <Link href={`/${item.name}`} className={styles.link}> { item.name } </Link>
+                            <div className={styles.liHeader}>
+                                <Link href={`/${item.name}`} className={styles.link}> { item.name } </Link>
+
+                                {
+                                    item.children.length > 0 &&
+                                    <FontAwesomeIcon
+                                        icon={faCaretDown}
+                                        rotation={item.toggled ? 270 : 0}
+                                        onClick={() => toggleNavItemChildren(item.name)}
+                                    />
+                                }
+                            </div>
+
                             {
-                                (item.children.length > 0) &&
-                                <ul>
+                                (item.children.length > 0 && item.toggled) &&
+                                <ul className={styles.childrenList}>
                                     {
-                                        item.children.map((child) => (<li key={child}> <Link href={`/${child}`} className={styles.link}> { child } </Link> </li>))
+                                        item.children.map((child) => (<li key={child} className={styles.li}> <Link href={`/${child}`} className={styles.link}> { child.replace('-', ' ') } </Link> </li>))
                                     }
                                 </ul>
                             }
