c50b18c0559cb0def798673c2c2643cb7ea7e22f
Antonio Sotillo <antoniosotillo14@hotmail.com>
2023-10-03 19:39:29 -0400

    Creacion de componente lista, adecuaciones de pagina para hacer las reutilizarla para todos los elementos de busqueda, creacion de servicio para obtener elementos de cualquier busqueda

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,38 @@
+import QueryFilters from "@/models/query-filters";
+import { fetchElements } from "@/services/list";
 import { useState } from "react"
 
 export default function SearchBar({
     uriSegment = "",
+    setElements,
+    setError,
 }) {
     const [inputValue, setInputValue] = useState("");
-    const handleSubmit = (e) => {
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        
+
+        try {
+            const response = await fetchElements(uriSegment,);
+
+            if (!response.ok) throw new Error(response.status, response.statusText);
+
+            let { results } = await response.json();
+
+            results = results.filter(({ name }) => name.startsWith(inputValue.trim()));
+
+            setElements(results);
+        } catch (err) {
+            // setError(err.message);
+            console.error(err)
+        }
     }
+
     const handleReset = (e) => {
         setInputValue("");
     }
 
-    if (!uriSegment.trim()) return <p>Wrong URI. Try with /pokemon, /item...</p>
+    if (!uriSegment.trim()) return <></>
 
     return (
         <form autoComplete="off" onSubmit={handleSubmit} onReset={handleReset}>
--- src/models/query-filters.js
+++ src/models/query-filters.js
@@ -1,7 +1,7 @@
 export default class QueryFilters {
     constructor({
         offset = 0,
-        limit = 10
+        limit = 1300,
     }) {
         this.offset = offset;
         this.limit = limit;
